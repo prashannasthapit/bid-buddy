28f7c804254b1595bedeca2d7ed726bdddd3199d
kami <prashannasthapit@outlook.com>
2024-06-28 15:14:14 +0545

    add in next-auth and items table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,44 @@
+import { auth } from "@/auth";
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { database } from "@/db/database";
-import { bids as bidsSchema } from "@/db/schema";
+import { items } from "@/db/schema";
 import { revalidatePath } from "next/cache";
 
 export default async function Home() {
-  const bids = await database.query.bids.findMany();
+  const session = await auth();
+
+  const allItems = await database.query.items.findMany();
+
+  // if (!session) return null;
+  // const user = session.user;
+  // if (!user) return null;
 
   return (
     <main className="container mx-auto py-12">
       Nǐ hǎo shìjiè
+      {!session ? <SignIn /> : <SignOut />}
+      {session?.user?.name}
       <form
         action={async (formData: FormData) => {
           "use server";
-          await database.insert(bidsSchema).values({});
+          await database.insert(items).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
           revalidatePath("/");
         }}
       >
-        <Input name="bid" placeholder="Bid" />
-        <Button type="submit">Place Bid</Button>
+        <Input name="name" placeholder="Name your item" />
+        <Button type="submit">Post Item</Button>
       </form>
-      {bids.map((bid) => (
-        <div key={bid.id}>{bid.id}</div>
+      {/* {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))} */}
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
       ))}
     </main>
   );
